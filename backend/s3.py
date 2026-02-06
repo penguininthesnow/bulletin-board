0aa47f4ad5ad9ed62e20e0cd69719e981a77b5dc
Chow <chouuu0976286138@gmail.com>
2026-02-06 09:49:35 +0800

    update env and s3 config

--- backend/s3.py
+++ backend/s3.py
@@ -4,9 +4,9 @@ import os
 
 s3 = boto3.client(
     "s3",
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
     region_name=os.getenv("AWS_REGION"),
+    # aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    # aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
 )
 
 BUCKET = os.getenv("S3_BUCKET_NAME")
